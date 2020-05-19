cdcc619d809f78256b84fbae86e090ccd880fbc3
PythonGuy <grifmang@gmail.com>
2020-05-18 21:57:49 -0400

    Update marioHacker.py

--- marioHacker.py
+++ marioHacker.py
@@ -15,3 +15,24 @@ while counter < int(height):
     hash += 1
     counter += 1
     row -= 1
+    
+# Python 3 update
+'''
+hash = 1
+counter = 0
+height = int(input("Enter the height of your mario wall (1-23): "))
+row = int(height)
+if height < 0:
+    print("Please enter a height greater than zero.")
+if height > 23:
+    print("Please enter a height of 23 or less.")
+while counter < int(height):
+    print(" " * row, \
+          "#" * hash, \
+          " ", \
+          "#" * hash, \
+          " " * row)
+    hash += 1
+    counter += 1
+    row -= 1
+'''
